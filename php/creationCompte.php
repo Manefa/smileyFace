2a22cf6149d17c59f4a0e152ac3873e291663039
Gabriel <sbrubicon12@hotmail.com>
2023-09-12 17:22:24 -0400

    ajout bases

--- php/creationCompte.php
+++ php/creationCompte.php
@@ -7,6 +7,12 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                
+            </div>
+        </div>
+    </div>
 </body>
 </html>
