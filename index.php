e53bfc506a8a7f52ea81ccf87c714cba8d3a3e40
Manefa <manefae8@gmail.com.com>
2023-09-15 10:01:37 -0400

    index page

--- index.php
+++ index.php
@@ -14,40 +14,46 @@
 
     <div class="container-fluid">
         <div class="row">
+            <div class="col-md-4 mt-4 ms-4 d-flex flex-row align-items-center">
+                <img src="assets/logo.svg" width="70" height="70" alt="logo">
+                <h3 class="ms-5 fw-bold ">Cegep Tr</h3>
+            </div>
 
         </div>
         <div class="row">
 
-            <div class="col-md-12 mt-5 text-center">
-                <h1>Avez vous aprecie l'evenement ?</h1>
+            <div class="row mt-5 d-flex align-items-center justify-content-center">
+                <div class="col-md-4 text-center">
+                    <h1 class="fw-bold">Choissizez le profil qui va voter</h1>
+                </div>
+
             </div>
 
-
-    
-
-        </div>
-        <div class="row mt-5">
-            <div class=" d-flex justify-content-center">
-                <div class="col-md-2 col-sl-2 d-flex justify-content-center">
-                    <img src="assets/grinning-squinting-face.svg" alt="" class="" srcset="">
+            <div class="row mt-5 d-flex align-items-center justify-content-center">
+                <div class="col-md-6 text-center">
+                    <img src="assets/knowledge-c.svg" alt="etudiant">
+                    <h1 class="fw-bold">Etudiant</h1>
                 </div>
-                <div class="col-md-2 col-sl-2 d-flex justify-content-center">
-                    <img src="assets/expressionless-face.svg" alt="" class="" srcset="">
+                <div class="col-md-6 text-center">
+                    <img src="assets/career-52.svg" alt="etudiant">
+                    <h1 class="fw-bold">Employeur</h1>
                 </div>
-                <div class="col-md-2 col-sl-2 d-flex justify-content-center">
-                    <img src="assets/pouting-face.svg" alt="" class="" srcset="">
-                </div>
-            </div>
 
-        </div>
-        <div class="row ">
-            <div class="col-md-8 bg-primary">
-            <button type="button" class="btn btn-secondary btn-md" >Secondary</button>
             </div>
         </div>
-    </div>
+        <div class="row d-flex bg-primary  align-items-center justify-content-center">
+            <div class="w-[500px] h-[100px] relative">
+                <div class="w-[500px] h-[100px] left-0 top-0 absolute bg-sky-900 rounded-lg"></div>
+                <div class="left-[112px] top-[19px] absolute text-white text-5xl font-bold font-['Oxygen']">Commencer</div>
+            </div>
+            <div class="col-md-6">
+                <div class="col-md-4"></div>
+                div
+            </div>
+        </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 
 </html>
