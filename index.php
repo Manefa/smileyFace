e66e11bf80bc366cfec123a94f00afbeebb6371b
Manefa <manefae8@gmail.com.com>
2023-09-15 08:50:52 -0400

    add smileface project

--- index.php
+++ index.php
@@ -11,10 +11,39 @@
 </head>
 
 <body>
-    
+
+    <div class="container-fluid">
+        <div class="row">
+
+        </div>
+        <div class="row">
+
+            <div class="col-md-12 mt-5 text-center">
+                <h1>Avez vous aprecie l'evenement ?</h1>
+            </div>
 
 
+        </div>
+        <div class="row mt-5">
+            <div class=" d-flex justify-content-center">
+                <div class="col-md-2 col-sl-2 d-flex justify-content-center">
+                    <img src="assets/grinning-squinting-face.svg" alt="" class="" srcset="">
+                </div>
+                <div class="col-md-2 col-sl-2 d-flex justify-content-center">
+                    <img src="assets/expressionless-face.svg" alt="" class="" srcset="">
+                </div>
+                <div class="col-md-2 col-sl-2 d-flex justify-content-center">
+                    <img src="assets/pouting-face.svg" alt="" class="" srcset="">
+                </div>
+            </div>
 
+        </div>
+        <div class="row ">
+            <div class="col-md-8 bg-primary">
+            <button type="button" class="btn btn-secondary btn-md" >Secondary</button>
+            </div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
