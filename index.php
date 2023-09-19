70414dcd6c428273de78d85cfbc8f3d701d79ce2
manefa <manefae8@gmail.com>
2023-09-19 17:51:54 -0400

    add index and evaluation page

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/custumBouton.css">
     <title>Index</title>
 </head>
 
@@ -16,40 +16,39 @@
         <div class="row">
             <div class="col-md-4 mt-4 ms-4 d-flex flex-row align-items-center">
                 <img src="assets/logo.svg" width="70" height="70" alt="logo">
-                <h3 class="ms-5 fw-bold ">Cegep Tr</h3>
+                <h1 class="ms-5 fw-bold ">Cegep Tr</h1>
             </div>
 
         </div>
         <div class="row">
 
-            <div class="row mt-5 d-flex align-items-center justify-content-center">
-                <div class="col-md-4 text-center">
+            <div class="row mt-4 d-flex align-items-center justify-content-center">
+                <div class="col-md-3 text-center">
                     <h1 class="fw-bold">Choissizez le profil qui va voter</h1>
                 </div>
 
             </div>
 
-            <div class="row mt-5 d-flex align-items-center justify-content-center">
-                <div class="col-md-6 text-center">
-                    <img src="assets/knowledge-c.svg" alt="etudiant">
+            <div class="row mt-4 d-flex align-items-center justify-content-center">
+                <div class="col-md-5 text-center">
+                    <img class="img-fluid" height="60%" width="60%" src="assets/boy on graduation-cuate.svg" alt="etudiant">
                     <h1 class="fw-bold">Etudiant</h1>
                 </div>
-                <div class="col-md-6 text-center">
-                    <img src="assets/career-52.svg" alt="etudiant">
+                <div class="col-md-5 text-center">
+                    <img class="img-fluid" height="60%" width="60%" class="img-fluid" src="assets/Business deal-cuate.svg" alt="etudiant">
                     <h1 class="fw-bold">Employeur</h1>
                 </div>
 
             </div>
         </div>
-        <div class="row d-flex bg-primary  align-items-center justify-content-center">
-            <div class="w-[500px] h-[100px] relative">
-                <div class="w-[500px] h-[100px] left-0 top-0 absolute bg-sky-900 rounded-lg"></div>
-                <div class="left-[112px] top-[19px] absolute text-white text-5xl font-bold font-['Oxygen']">Commencer</div>
+        
+        <div class="row mt-5 d-flex justify-content-center align-items-center">
+            <div class="col-md-4 ">
+                <div href="#" class="btn btn-new d-flex justify-content-center align-items-center" >
+                    <h3><a class="me-0" style="color: white; text-decoration: none; " href="pages/evaluation.php">Commencer !</a></h3>
+                </div>
             </div>
-            <div class="col-md-6">
-                <div class="col-md-4"></div>
-                div
-            </div>
+
         </div>
 
 
---
+++ pages/evaluation.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Evaluation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/custumBouton.css">
+    <link rel="stylesheet" href="../css/title-css.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4 mt-4 ms-4 d-flex flex-row align-items-center">
+                <img src="../assets/logo.svg" width="70" height="70" alt="logo">
+                <h1 class="ms-5 fw-bold ">Cegep Tr</h1>
+            </div>
+
+        </div>
+
+
+        <div class="row mt-5 d-flex align-items-center justify-content-center">
+            <div class="col-md-4 text-center">
+                <h1 class="fw-bold" id="textIntro">Avez vous apprecier l’evenement </h1>
+            </div>
+
+        </div>
+
+        <div class="row mt-5 d-flex align-items-center justify-content-center">
+            <div class="col-md-4 text-center d-flex justify-content-end">
+                <img class="img-fluid" height="60%" width="60%" src="../assets/face_amazed.svg" alt="etudiant">
+
+            </div>
+            <div class="col-md-4 text-center">
+                <img class="img-fluid" height="60%" width="60%" class="img-fluid" src="../assets/face_neutral.svg" alt="etudiant">
+
+            </div>
+
+            <div class="col-md-4 text-center d-flex justify-content-start">
+                <img class="img-fluid" height="60%" width="60%" class="img-fluid" src="../assets/face_angry.svg" alt="etudiant">
+
+            </div>
+
+        </div>
+
+        <div class="row mt-5 d-flex justify-content-center align-items-center">
+            <div class="col-md-4 ">
+                <div href="#" class="btn btn-new d-flex justify-content-center align-items-center">
+                    <h3><a class="me-0" id="linkText" style="color: white; text-decoration: none; " href="pages/evaluation.php">Evaluer !</a></h3>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+
+</html>
