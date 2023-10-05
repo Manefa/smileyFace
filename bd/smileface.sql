ea6bf336e8570a758601c91333eecff39829258a
Manefa <manefae8@gmail.com>
2023-10-04 22:54:17 -0400

    new bd

--- bd/smileface.sql
+++ bd/smileface.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mer 04 Octobre 2023 à 02:01
+-- Généré le :  Jeu 05 Octobre 2023 à 02:53
 -- Version du serveur :  5.7.11
--- Version de PHP :  7.0.3
+-- Version de PHP :  5.6.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -57,17 +57,6 @@ CREATE TABLE `employeesatisfaction` (
 --
 
 INSERT INTO `employeesatisfaction` (`idEm`, `satisfactionlevelEm`, `idEv`) VALUES
-(13, 2, 30),
-(14, 3, 30),
-(15, 2, 30),
-(16, 1, 30),
-(17, 2, 30),
-(18, 3, 30),
-(19, 3, 30),
-(20, 2, 30),
-(21, 1, 30),
-(22, 2, 30),
-(23, 3, 30),
 (24, 2, 50),
 (25, 3, 50),
 (26, 2, 50),
@@ -88,7 +77,13 @@ INSERT INTO `employeesatisfaction` (`idEm`, `satisfactionlevelEm`, `idEv`) VALUE
 (41, 3, 50),
 (42, 3, 50),
 (43, 2, 50),
-(44, 1, 50);
+(44, 1, 50),
+(45, 2, 50),
+(46, 3, 50),
+(47, 2, 50),
+(48, 1, 50),
+(49, 2, 50),
+(50, 3, 50);
 
 -- --------------------------------------------------------
 
@@ -112,12 +107,9 @@ CREATE TABLE `event` (
 --
 
 INSERT INTO `event` (`idEv`, `nameEv`, `dateEv`, `timeEv`, `locationEv`, `employeurEv`, `descriptionEv`, `idUser`) VALUES
-(30, 'Danse', '2023-10-19', '12:30 ', 'SA2090', 'GOX', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 2),
-(46, 'fsd', '2023-10-05', '19:35', 'fsd', 'fsd', 'fds', 2),
-(47, 'fdg', '2023-10-13', '17:50', 'gdf', 'gdf', 'gdf', 2),
-(48, 'fsd', '2023-10-12', '12:50', 'fsd', 'fds', 'fsd', 2),
-(49, 'fsd', '2023-10-05', '17:50', 'SA2090', 'fds', 'high quality products', 2),
-(50, 'fsd', '2023-10-20', '17:50', 'SA2090', 'fds', 'das', 2);
+(50, 'Danse', '2023-10-20', '17:50 ', 'SA2090', 'fds', 'das', 2),
+(51, 'das', '2023-10-27', '12:00', 'das', 'das', 'das', 2),
+(52, 'das', '2023-10-20', '17:40', 'das', 'das', 'das', 2);
 
 -- --------------------------------------------------------
 
@@ -136,15 +128,9 @@ CREATE TABLE `liason` (
 --
 
 INSERT INTO `liason` (`id`, `idEv`, `idDpt`) VALUES
-(19, 30, 2),
-(20, 30, 1),
-(21, 30, 1),
-(31, 46, 2),
-(32, 46, 1),
-(33, 47, 1),
-(34, 48, 1),
-(35, 49, 1),
-(36, 50, 2);
+(36, 50, 2),
+(37, 51, 1),
+(38, 52, 2);
 
 -- --------------------------------------------------------
 
@@ -163,56 +149,43 @@ CREATE TABLE `studentsatisfaction` (
 --
 
 INSERT INTO `studentsatisfaction` (`idEt`, `satisfactionlevelEt`, `idEv`) VALUES
-(7, 1, 30),
-(8, 1, 30),
-(9, 1, 30),
-(10, 1, 30),
-(11, 1, 30),
-(12, 1, 30),
-(13, 1, 30),
-(14, 1, 30),
-(15, 1, 30),
-(16, 2, 30),
-(17, 1, 30),
-(18, 3, 30),
-(19, 3, 30),
-(20, 3, 30),
-(21, 1, 30),
-(22, 1, 30),
-(23, 2, 30),
-(24, 2, 30),
-(25, 3, 30),
-(26, 2, 30),
-(27, 1, 30),
-(28, 1, 30),
-(29, 2, 30),
-(30, 3, 30),
-(31, 2, 30),
-(32, 3, 30),
-(33, 1, 30),
-(34, 2, 30),
-(35, 2, 30),
-(36, 3, 30),
-(37, 2, 30),
-(38, 1, 30),
-(39, 1, 30),
-(40, 2, 30),
-(41, 3, 30),
-(42, 2, 30),
-(43, 1, 30),
-(44, 1, 30),
-(45, 1, 30),
-(46, 2, 30),
-(47, 3, 30),
-(48, 3, 30),
-(49, 2, 30),
-(50, 3, 30),
-(51, 1, 30),
-(52, 2, 30),
-(53, 1, 30),
-(54, 3, 30),
-(55, 2, 30),
-(56, 1, 30);
+(57, 1, 50),
+(58, 2, 50),
+(59, 3, 50),
+(60, 2, 50),
+(61, 2, 50),
+(62, 2, 50),
+(63, 2, 50),
+(64, 2, 50),
+(65, 2, 50),
+(66, 2, 50),
+(67, 3, 50),
+(68, 1, 50),
+(69, 3, 50),
+(70, 1, 50),
+(71, 2, 50),
+(72, 2, 50),
+(73, 2, 50),
+(74, 2, 50),
+(75, 2, 50),
+(76, 1, 50),
+(77, 1, 50),
+(78, 2, 50),
+(79, 2, 50),
+(80, 2, 50),
+(81, 2, 50),
+(82, 1, 50),
+(83, 1, 50),
+(84, 1, 50),
+(85, 1, 50),
+(86, 2, 50),
+(87, 2, 50),
+(88, 3, 50),
+(89, 3, 50),
+(90, 1, 50),
+(91, 1, 50),
+(92, 2, 50),
+(93, 2, 50);
 
 -- --------------------------------------------------------
 
@@ -237,7 +210,6 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`idUser`, `image`, `lastname`, `firstname`, `poste`, `email`, `password`, `pin`) VALUES
 (2, 'https://pnganime.com/web/images/l/luffy-gear-5-colored.png', 'LUFFY', 'fds', 'fds', 'fsd@gmail.com', '8cb2237d0679ca88db6464eac60da96345513964', 1234),
-(4, 'https://assets.stickpng.com/images/584e837f6a5ae41a83ddee3b.png', 'fewf', 'weew', 'rwe', 'fds@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 1234),
 (6, 'https://pnganime.com/web/images/l/luffy-gear-5-colored.png', 'aaaaaaaaaaaaaa', 'fds', 'fds', 'fsd@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 1234);
 
 --
@@ -298,22 +270,22 @@ ALTER TABLE `departement`
 -- AUTO_INCREMENT pour la table `employeesatisfaction`
 --
 ALTER TABLE `employeesatisfaction`
-  MODIFY `idEm` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `idEm` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 --
 -- AUTO_INCREMENT pour la table `event`
 --
 ALTER TABLE `event`
-  MODIFY `idEv` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `idEv` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 --
 -- AUTO_INCREMENT pour la table `liason`
 --
 ALTER TABLE `liason`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 --
 -- AUTO_INCREMENT pour la table `studentsatisfaction`
 --
 ALTER TABLE `studentsatisfaction`
-  MODIFY `idEt` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `idEt` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
 --
 -- AUTO_INCREMENT pour la table `user`
 --
