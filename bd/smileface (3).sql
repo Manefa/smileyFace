59219c0281c43cb6811d7bbe445318861d71e568
Manefa <manefae8@gmail.com>
2023-10-05 17:03:47 -0400

    new bd

---
+++ bd/smileface (3).sql
@@ -0,0 +1,389 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Jeu 05 Octobre 2023 à 21:03
+-- Version du serveur :  5.7.11
+-- Version de PHP :  7.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `smileface`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `departement`
+--
+
+CREATE TABLE `departement` (
+  `id` int(11) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `Name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `departement`
+--
+
+INSERT INTO `departement` (`id`, `code`, `Name`) VALUES
+(1, '420.BO', 'Techniques de l informatique'),
+(2, '241.A0', 'Techniques de genie mecanique');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `employeesatisfaction`
+--
+
+CREATE TABLE `employeesatisfaction` (
+  `idEm` int(11) NOT NULL,
+  `satisfactionlevelEm` int(11) NOT NULL,
+  `idEv` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `employeesatisfaction`
+--
+
+INSERT INTO `employeesatisfaction` (`idEm`, `satisfactionlevelEm`, `idEv`) VALUES
+(24, 2, 50),
+(25, 3, 50),
+(26, 2, 50),
+(27, 1, 50),
+(28, 1, 50),
+(29, 2, 50),
+(30, 3, 50),
+(31, 3, 50),
+(32, 3, 50),
+(33, 3, 50),
+(34, 2, 50),
+(35, 2, 50),
+(36, 1, 50),
+(37, 1, 50),
+(38, 2, 50),
+(39, 3, 50),
+(40, 3, 50),
+(41, 3, 50),
+(42, 3, 50),
+(43, 2, 50),
+(44, 1, 50),
+(45, 2, 50),
+(46, 3, 50),
+(47, 2, 50),
+(48, 1, 50),
+(49, 2, 50),
+(50, 3, 50),
+(51, 2, 54),
+(52, 2, 54),
+(53, 3, 54),
+(54, 3, 54),
+(55, 1, 54),
+(56, 1, 54);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `event`
+--
+
+CREATE TABLE `event` (
+  `idEv` int(11) NOT NULL,
+  `nameEv` varchar(255) NOT NULL,
+  `dateEv` date NOT NULL,
+  `timeEv` varchar(255) NOT NULL,
+  `locationEv` varchar(255) CHARACTER SET ucs2 NOT NULL,
+  `employeurEv` varchar(255) NOT NULL,
+  `descriptionEv` varchar(255) NOT NULL,
+  `idUser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `event`
+--
+
+INSERT INTO `event` (`idEv`, `nameEv`, `dateEv`, `timeEv`, `locationEv`, `employeurEv`, `descriptionEv`, `idUser`) VALUES
+(50, 'Danse', '2023-10-20', '17:50 ', 'SA2090', 'fds', 'das', 2),
+(53, 'fds', '2023-10-20', '18:50', 'fds', 'fds', 'fds', 2),
+(54, 'fds', '2023-10-13', '13:45', 'fsd', 'fds', 'fsd', 2),
+(55, 'fsd', '2023-10-06', '13:40', 'fsd', 'fsd', 'fsd', 2),
+(56, 'wwwww', '2023-10-14', '18:00', 'wwwww', 'w', 'w', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `liason`
+--
+
+CREATE TABLE `liason` (
+  `id` int(11) NOT NULL,
+  `idEv` int(11) NOT NULL,
+  `idDpt` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `liason`
+--
+
+INSERT INTO `liason` (`id`, `idEv`, `idDpt`) VALUES
+(36, 50, 2),
+(39, 53, 1),
+(40, 53, 2),
+(41, 54, 1),
+(42, 54, 2),
+(43, 55, 1),
+(44, 56, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `studentsatisfaction`
+--
+
+CREATE TABLE `studentsatisfaction` (
+  `idEt` int(11) NOT NULL,
+  `satisfactionlevelEt` int(11) NOT NULL,
+  `idEv` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `studentsatisfaction`
+--
+
+INSERT INTO `studentsatisfaction` (`idEt`, `satisfactionlevelEt`, `idEv`) VALUES
+(57, 1, 50),
+(58, 2, 50),
+(59, 3, 50),
+(60, 2, 50),
+(61, 2, 50),
+(62, 2, 50),
+(63, 2, 50),
+(64, 2, 50),
+(65, 2, 50),
+(66, 2, 50),
+(67, 3, 50),
+(68, 1, 50),
+(69, 3, 50),
+(70, 1, 50),
+(71, 2, 50),
+(72, 2, 50),
+(73, 2, 50),
+(74, 2, 50),
+(75, 2, 50),
+(76, 1, 50),
+(77, 1, 50),
+(78, 2, 50),
+(79, 2, 50),
+(80, 2, 50),
+(81, 2, 50),
+(82, 1, 50),
+(83, 1, 50),
+(84, 1, 50),
+(85, 1, 50),
+(86, 2, 50),
+(87, 2, 50),
+(88, 3, 50),
+(89, 3, 50),
+(90, 1, 50),
+(91, 1, 50),
+(92, 2, 50),
+(93, 2, 50),
+(94, 2, 53),
+(95, 2, 53),
+(96, 3, 53),
+(97, 3, 53),
+(98, 1, 53),
+(99, 1, 53),
+(100, 3, 53),
+(101, 3, 53),
+(102, 1, 53),
+(103, 2, 53),
+(104, 3, 53),
+(105, 1, 53),
+(106, 3, 53),
+(107, 2, 53),
+(108, 1, 54),
+(109, 1, 54),
+(110, 2, 54),
+(111, 2, 54),
+(112, 3, 54),
+(113, 3, 54),
+(114, 1, 55),
+(115, 1, 55),
+(116, 2, 55),
+(117, 2, 55),
+(118, 3, 55),
+(119, 3, 55),
+(120, 2, 53),
+(121, 2, 53),
+(122, 1, 53),
+(123, 1, 53),
+(124, 3, 53),
+(125, 3, 53),
+(126, 3, 53),
+(127, 3, 53),
+(128, 3, 53),
+(129, 3, 53),
+(130, 3, 53),
+(131, 3, 53),
+(132, 2, 55),
+(133, 2, 55),
+(134, 3, 55),
+(135, 3, 55),
+(136, 1, 55),
+(137, 1, 55),
+(138, 1, 55),
+(139, 1, 55),
+(140, 1, 55),
+(141, 1, 55),
+(142, 1, 55),
+(143, 3, 55),
+(144, 1, 55),
+(145, 1, 55);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `idUser` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `lastname` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `firstname` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `poste` varchar(255) NOT NULL,
+  `email` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `password` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `pin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`idUser`, `image`, `lastname`, `firstname`, `poste`, `email`, `password`, `pin`) VALUES
+(2, 'https://pnganime.com/web/images/l/luffy-gear-5-colored.png', 'LUFFY', 'fds', 'fds', 'fsd@gmail.com', '8cb2237d0679ca88db6464eac60da96345513964', 123466),
+(6, 'https://pnganime.com/web/images/l/luffy-gear-5-colored.png', 'aaaaaaaaaaaaaa', 'fds', 'fds', 'fsd@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 123466);
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `departement`
+--
+ALTER TABLE `departement`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `employeesatisfaction`
+--
+ALTER TABLE `employeesatisfaction`
+  ADD PRIMARY KEY (`idEm`),
+  ADD KEY `idEv` (`idEv`);
+
+--
+-- Index pour la table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`idEv`),
+  ADD KEY `idUser` (`idUser`);
+
+--
+-- Index pour la table `liason`
+--
+ALTER TABLE `liason`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idEvent` (`idEv`),
+  ADD KEY `idDept` (`idDpt`);
+
+--
+-- Index pour la table `studentsatisfaction`
+--
+ALTER TABLE `studentsatisfaction`
+  ADD PRIMARY KEY (`idEt`),
+  ADD KEY `idEv` (`idEv`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`idUser`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `departement`
+--
+ALTER TABLE `departement`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `employeesatisfaction`
+--
+ALTER TABLE `employeesatisfaction`
+  MODIFY `idEm` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+--
+-- AUTO_INCREMENT pour la table `event`
+--
+ALTER TABLE `event`
+  MODIFY `idEv` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+--
+-- AUTO_INCREMENT pour la table `liason`
+--
+ALTER TABLE `liason`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+--
+-- AUTO_INCREMENT pour la table `studentsatisfaction`
+--
+ALTER TABLE `studentsatisfaction`
+  MODIFY `idEt` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=146;
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `employeesatisfaction`
+--
+ALTER TABLE `employeesatisfaction`
+  ADD CONSTRAINT `fk_employeesatisfaction_event` FOREIGN KEY (`idEv`) REFERENCES `event` (`idEv`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `event`
+--
+ALTER TABLE `event`
+  ADD CONSTRAINT `fk_event_user` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`);
+
+--
+-- Contraintes pour la table `liason`
+--
+ALTER TABLE `liason`
+  ADD CONSTRAINT `fk_liason_departement` FOREIGN KEY (`idDpt`) REFERENCES `departement` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_liason_event` FOREIGN KEY (`idEv`) REFERENCES `event` (`idEv`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `studentsatisfaction`
+--
+ALTER TABLE `studentsatisfaction`
+  ADD CONSTRAINT `fk_studentsatisfaction_event` FOREIGN KEY (`idEv`) REFERENCES `event` (`idEv`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
